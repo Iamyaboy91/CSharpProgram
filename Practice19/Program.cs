1d0531d6aaa4173e1f0ce6d76ad29b16b57e4be6
Takaaki Saito <takaaki.skater@gmail.com>
2024-05-01 11:42:52 +0900

    do while if elseif case challenge

---
+++ Practice19/Program.cs
@@ -0,0 +1,27 @@
+﻿string? readResult;
+string enteredNum = "";
+bool validNunmber = false;
+int numValue = 0;
+Console.WriteLine("Enter an integer value between 5 and 10");
+do
+{ 
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        enteredNum = readResult;
+    }
+    validNunmber = int.TryParse(enteredNum, out numValue);
+    if(validNunmber == true)
+    {
+        if (numValue <= 5 || numValue >= 10)
+        {
+            validNunmber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }else 
+    {
+    Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+}while(validNunmber == false);
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+readResult = Console.ReadLine();
---
+++ Practice21/Program.cs
@@ -0,0 +1,22 @@
+﻿string? readResult;
+string roleName = ""; 
+bool enteredName = false;
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+do
+{
+    readResult = Console.ReadLine();
+    if( readResult != null )
+    {
+        roleName = readResult.Trim();
+    }
+
+    if(roleName.ToLower() == "administrator" || roleName.ToLower() == "manger" || roleName.ToLower() == "user")
+        {
+            enteredName = true;
+        }else
+        {
+            Console.WriteLine($"The role name that you entered, \"{roleName}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+        }
+}while(enteredName == false);
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+readResult = Console.ReadLine();
